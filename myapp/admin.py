b9a33de7e2b85a88a1306da01e943576500e33bf
unknown <noy@gmail.com>
2025-08-14 11:51:53 +0700

    first commit1

--- myapp/admin.py
+++ myapp/admin.py
@@ -20,7 +20,7 @@ class SubscriptionResource(resources.ModelResource):
             'id', 'name', 'gender', 'name_eng', 'age','profile','Skills','birthday', 'email', 'province',
             'province_school','districts_school','village_school','Mo','Language','Language1','Language2','Nationality',
             'districts', 'village', 'tel', 'from_school', 'academic_year','Religion','Other_Skill',
-            'employee', 'semester', 'status', 'registered_at', 'subject_names'
+            'employee', 'semester', 'status', 'registered_at', 'subject_names','Language_Level','Language_Level1','Skill_full'
         )
 
 # Subscription Admin
