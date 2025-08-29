a1718f598820c36b05fa435ab7f4ef534d76c70e
unknown <noy@gmail.com>
2025-08-29 15:34:49 +0700

    nnn

--- myapp/admin.py
+++ myapp/admin.py
@@ -28,9 +28,10 @@ class SubscriptionResource(resources.ModelResource):
 class SubscriptionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     resource_class = SubscriptionResource
     list_display = [
-        'id','StudentID','gender', 'name','photo', 'name_eng','age', 'birthday', 'email', 'province','Facebook','subject',
-        'districts', 'Current_village','Current_village','Current_districts','province', 'tel', 'from_school', 'academic_year',
-        'employee', 'semester', 'status', 'registered_at'
+        'id', 'name', 'gender', 'name_eng', 'age','profile','Skills','birthday', 'email', 'province',
+            'province_school','districts_school','village_school','Mo','Language','Language1','Language2','Nationality','Parents_contact',
+            'districts', 'village', 'tel', 'from_school', 'academic_year','Religion','Other_Skill','Ability1','Ability2','Ability3','Work','School'
+            'employee', 'semester', 'status', 'registered_at', 'subject_names','Language_Level','Language_Level2','Skill_full','Detail_Skill'
     ]
     search_fields = ['name', 'email']
     list_filter = ['status']
@@ -47,5 +48,4 @@ class SubjectAdmin(ImportExportModelAdmin, admin.ModelAdmin):
 # Register
 admin.site.register(Subscription, SubscriptionAdmin)
 admin.site.register(Subject, SubjectAdmin)
-
 admin.site.register(MyModel, fields=('my_image_thumbnail',), readonly_fields=('my_image_thumbnail',))
